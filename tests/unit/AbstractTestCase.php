d0c55fc731a22a5d361937527551ac945ab816ce
samizdam <samizdam@inbox.ru>
2017-04-01 22:56:16 +0300

    Fix spy type hinting.

--- tests/unit/AbstractTestCase.php
+++ tests/unit/AbstractTestCase.php
@@ -2,14 +2,14 @@
 
 namespace FreeElephants\HexammonServerTests;
 
-use PHPUnit_Framework_MockObject_Matcher_Invocation;
+use PHPUnit_Framework_MockObject_Matcher_InvokedRecorder;
 
 /**
  * @author samizdam <samizdam@inbox.ru>
  */
 class AbstractTestCase extends \PHPUnit_Framework_TestCase
 {
-    protected function getFirstInvocationFirstArgument(PHPUnit_Framework_MockObject_Matcher_Invocation $spy)
+    protected function getFirstInvocationFirstArgument(PHPUnit_Framework_MockObject_Matcher_InvokedRecorder $spy)
     {
         return $spy->getInvocations()[0]->parameters[0];
     }
