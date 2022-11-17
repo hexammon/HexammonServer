599b3271efe4df436eb171b48db54f47066389ff
samizdam <samizdam@inbox.ru>
2022-11-17 20:05:35 +0300

    Include game to room resource

--- src/Hexammon/Server/Infrastructure/Mapper/RoomMapper.php
+++ src/Hexammon/Server/Infrastructure/Mapper/RoomMapper.php
@@ -11,72 +11,24 @@ class RoomMapper
 {
     public function mapSingle(Room $room): array
     {
-        $isGameStarted = $room->isFilled();
-        $playersIncluded = array_map(function (NamedPlayer $player) {
-            return [
-                'type'       => 'players',
-                'id'         => $player->getId(),
-                'attributes' => [
-                    'name' => $player->getName(),
-                ],
-            ];
-        }, $room->getPlayers());
+        $includedPlayers = $this->getPlayersIncluded($room);
+        $includedGames = $this->getIncludedGames($room);
+        $included = array_merge($includedPlayers, $includedGames);
 
-        $document = [
-            'data' => $this->mapResource($room),
-            'included' => $playersIncluded,
+        return [
+            'data'     => $this->mapResource($room),
+            'included' => $included,
         ];
-
-        if ($isGameStarted) {
-
-            $game = $room->getGame();
-
-            $board = $game->getBoard();
-            $rows = [];
-            foreach ($board->getRows() as $row) {
-                $rows[$row->getNumber()] = array_map(function (AbstractTile $tile) {
-                    return [
-                        'hasCastle' => $tile->hasCastle(),
-                        'army'      => $tile->hasArmy() ? [
-                            'units'    => $tile->getArmy()->count(),
-                            'playerId' => $tile->getArmy()->getOwner()->getId(),
-                        ] : null,
-                    ];
-                }, $row->getTiles());
-            }
-
-            $columns = [];
-            foreach ($board->getColumns() as $column) {
-                $columns[$column->getNumber()] = array_map(function (AbstractTile $tile) {
-                    return [
-                        'hasCastle' => $tile->hasCastle(),
-                        'army'      => $tile->hasArmy() ? [
-                            'units'    => $tile->getArmy()->count(),
-                            'playerId' => $tile->getArmy()->getOwner()->getId(),
-                        ] : null,
-                    ];
-                }, $column->getTiles());
-            }
-
-            $document['included'][] = [
-                'type'       => 'games',
-                'attributes' => [
-                    'board' => [
-                        'type' => $board->getType(),
-                        'rows' => $rows,
-                        'cols' => $columns,
-                    ],
-                ],
-            ];
-        }
-
-        return $document;
     }
 
     public function mapCollection(array $rooms): array
     {
+        $includedPlayers = array_map([$this, 'getPlayersIncluded'], $rooms);
+        $includedGames = array_map([$this, 'getIncludedGames'], $rooms);
+        $included = array_merge($includedPlayers, $includedGames);
         return [
-            'data' => array_map([$this, 'mapResource'], $rooms)
+            'data'     => array_map([$this, 'mapResource'], $rooms),
+            'included' => $included,
         ];
     }
 
@@ -119,6 +71,75 @@ class RoomMapper
         }
 
         return $resource;
+    }
+
+    /**
+     * @param Room $room
+     * @return array|array[]
+     */
+    private function getPlayersIncluded(Room $room): array
+    {
+        $playersIncluded = array_map(function (NamedPlayer $player) {
+            return [
+                'type'       => 'players',
+                'id'         => $player->getId(),
+                'attributes' => [
+                    'name' => $player->getName(),
+                ],
+            ];
+        }, $room->getPlayers());
+        return $playersIncluded;
+    }
+
+    private function getIncludedGames(Room $room): array
+    {
+        $includedGame = [];
+        $isGameStarted = $room->isFilled();
+        if ($isGameStarted) {
+
+            $game = $room->getGame();
+
+            $board = $game->getBoard();
+            $rows = [];
+            foreach ($board->getRows() as $row) {
+                $rows[$row->getNumber()] = array_map(function (AbstractTile $tile) {
+                    return [
+                        'hasCastle' => $tile->hasCastle(),
+                        'army'      => $tile->hasArmy() ? [
+                            'units'    => $tile->getArmy()->count(),
+                            'playerId' => $tile->getArmy()->getOwner()->getId(),
+                        ] : null,
+                    ];
+                }, $row->getTiles());
+            }
+
+            $columns = [];
+            foreach ($board->getColumns() as $column) {
+                $columns[$column->getNumber()] = array_map(function (AbstractTile $tile) {
+                    return [
+                        'hasCastle' => $tile->hasCastle(),
+                        'army'      => $tile->hasArmy() ? [
+                            'units'    => $tile->getArmy()->count(),
+                            'playerId' => $tile->getArmy()->getOwner()->getId(),
+                        ] : null,
+                    ];
+                }, $column->getTiles());
+            }
+
+            $includedGame = [
+                'type'       => 'games',
+                'attributes' => [
+                    'board' => [
+                        'type' => $board->getType(),
+                        'rows' => $rows,
+                        'cols' => $columns,
+                    ],
+                ],
+            ];
+        }
+
+        return $includedGame;
+
     }
 
 }
